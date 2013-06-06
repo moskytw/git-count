b62be87ed8fc9ec2405fa0aaa5c026217b24cb08
Mosky <mosky.tw@gmail.com>
2013-06-07 01:48:10 +0800

    the first working version

---
+++ git-count.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pipes import quote
+from subprocess import Popen, PIPE
+from datetime import date, timedelta
+
+def shell(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    p.wait()
+    return p.stdout.read()[:-1]
+
+def count_git_log(**kargs):
+    shell_args = []
+    for k, v in kargs.items():
+        if isinstance(v, bool) and v:
+            shell_args.append('--%s' % k.replace('_', '-'))
+        elif v:
+            shell_args.append('--%s=%s' % (k.replace('_', '-'), quote(v)))
+    return int(shell('git log %s | wc -l' % ' '.join(shell_args)))
+
+DAY = timedelta(days=1)
+WEEK = timedelta(weeks=1)
+DATE_FORMAT  = '%Y/%m/%d'
+
+def count_cmd(period='weekly', number=6):
+
+    today = date.today()
+
+    if period.startswith('w'):
+        until = today - today.isoweekday()*DAY + WEEK
+    elif period.startswith('m'):
+        until = date(
+            today.year+(today.month+1 > 12),
+            today.month+1 % 12,
+            1
+        ) - DAY
+    elif period.startswith('y'):
+        until = date(today.year+1, 1, 1) - DAY
+
+    while number:
+
+        if period.startswith('w'):
+            since = until - WEEK + DAY
+        elif period.startswith('m'):
+            since = date(until.year, until.month, 1)
+        elif period.startswith('y'):
+            since = date(until.year, 1, 1)
+
+        print since, count_git_log(
+            oneline=True,
+            all=True,
+            no_merges=True,
+            since=since.strftime(DATE_FORMAT),
+            until=until.strftime(DATE_FORMAT),
+        )
+
+        until = since-DAY
+
+        number -= 1
+
+if __name__ == '__main__':
+    import clime
+    #clime.start(white_pattern=clime.CMD_SUFFIX, debug=True)
+    clime.start(white_pattern=clime.CMD_SUFFIX)

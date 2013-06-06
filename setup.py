e9dbf4f3eca994d928a391cddf8956a3f34fe20f
Mosky <mosky.tw@gmail.com>
2013-06-07 06:40:28 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+from gitcount import __version__
+
+setup(
+    name    = 'git-count',
+    description = 'It counts commits in days, weeks, months or years.',
+    long_description = open('README.rst').read(),
+    version = __version__,
+    author  = 'Mosky',
+    author_email = 'mosky.tw@gmail.com',
+    #url = 'http://git-count.mosky.tw/',
+    py_modules = ['gitcount'],
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development',
+    ],
+    entry_points = {
+         'console_scripts': [
+             'git-count = gitcount:main',
+        ]
+    },
+    install_requires = ['clime>=0.1.5'],
+)
+
